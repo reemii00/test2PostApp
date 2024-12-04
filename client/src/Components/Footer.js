ce277d5e38d5247be2c119e39471b87242757117
reemii00 <66j2014@utas.edu.om>
2024-12-04 14:43:47 +0400

    Update Footer.js First update

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>©2023.PostIT. All Rights Reserved.</div>
+      <div>©2023.PostIT. All Rights Reserved. Reem</div>
     </footer>
   );
 };
